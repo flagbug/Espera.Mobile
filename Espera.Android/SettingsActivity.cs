60708169fe7cbee3917054c10f22981a61002642
Dennis Daume <daume.dennis@gmail.com>
2013-11-08 14:12:56 +0100

    Validate the port

--- Espera.Android/SettingsActivity.cs
+++ Espera.Android/SettingsActivity.cs
@@ -1,13 +1,21 @@
 using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Preferences;
 using Android.Text;
+using System;
+using System.Linq;
 
 namespace Espera.Android
 {
     [Activity(Label = "Settings")]
     public class SettingsActivity : PreferenceActivity
     {
+        public void OnSharedPreferenceChanged(ISharedPreferences sharedPreferences, string key)
+        {
+            throw new NotImplementedException();
+        }
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -16,6 +24,25 @@ namespace Espera.Android
 
             var pref = (EditTextPreference)this.FindPreference("preference_port");
             pref.EditText.InputType = InputTypes.ClassNumber;
+            pref.EditText.TextChanged += (sender, args) =>
+            {
+                int port = Int32.Parse(new string(args.Text.ToArray()));
+
+                if (!IsValidPort(port))
+                {
+                    pref.EditText.Error = "Port must be between 49152 and 65535";
+                }
+            };
+            pref.PreferenceChange += (sender, args) =>
+            {
+                int port = Int32.Parse(args.NewValue.ToString());
+                args.Handled = IsValidPort(port);
+            };
+        }
+
+        private static bool IsValidPort(int port)
+        {
+            return port > 49152 && port < 65535;
         }
     }
 }

61ea217e412d19ce61e5ba603d0ff464d9492fdd
Dennis Daume <daume.dennis@gmail.com>
2014-06-20 16:16:10 +0200

    Tweak the build

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -3,7 +3,7 @@ $MSBuildLocation = "C:\Program Files (x86)\MSBuild\12.0\bin"
 & "$MSBuildLocation\MSBuild.exe" Espera.Android\Espera.Android.csproj /p:Configuration=Release /t:Clean
 & "$MSBuildLocation\MSBuild.exe" Espera.Android\Espera.Android.csproj /p:Configuration=Release /t:PackageForAndroid
 
-& 'C:\Program Files (x86)\Java\jdk1.6.0_39\bin\jarsigner.exe' -verbose -sigalg SHA1withRSA -digestalg SHA1  -keystore ./Espera.keystore -signedjar ./Espera.Android/bin/Release/Espera.Android-signed.apk ./Espera.Android/bin/Release/Espera.Android.apk Espera
+& 'C:\Program Files (x86)\Java\jdk1.6.0_39\bin\jarsigner.exe' -verbose -sigalg SHA1withRSA -digestalg SHA1  -keystore ./Espera.keystore -signedjar ./Espera.Android/bin/Release/com.flagbug.esperamobile-signed.apk ./Espera.Android/bin/Release/com.flagbug.esperamobile.apk Espera
 
-mkdir ./Release
-& 'C:\Program Files (x86)\Android\android-sdk\tools\zipalign.exe' -f -v 4 ./Espera.Android/bin/Release/Espera.Android-signed.apk ./Release/Espera.Android.apk
+mkdir -Force ./Release
+& 'C:\Program Files (x86)\Android\android-sdk\tools\zipalign.exe' -f -v 4 ./Espera.Android/bin/Release/com.flagbug.esperamobile-signed.apk ./Release/Espera.Android.apk

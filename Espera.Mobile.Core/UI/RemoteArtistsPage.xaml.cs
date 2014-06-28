04d8289a21df201d3103c6e04a76c04af8b7ceae
Dennis Daume <daume.dennis@gmail.com>
2014-06-28 15:28:31 +0200

    RxUI has a bug with XAML bindings, use RxUI bindings for now

--- Espera.Mobile.Core/UI/RemoteArtistsPage.xaml.cs
+++ Espera.Mobile.Core/UI/RemoteArtistsPage.xaml.cs
@@ -20,6 +20,8 @@ namespace Espera.Mobile.Core.UI
             this.ViewModel = new ArtistsViewModel<RemoteSong>(new RemoteSongFetcher(), BlobCacheKeys.SelectedRemoteSongs);
             this.BindingContext = this.ViewModel;
 
+            this.ViewModel.WhenAnyValue(x => x.Artists).BindTo(this.ArtistsListView, x => x.ItemsSource);
+
             this.ViewModel.LoadCommand.IsExecuting
                 .BindTo(this.LoadIndicator, x => x.IsVisible);
             this.ViewModel.LoadCommand.IsExecuting
@@ -36,6 +38,8 @@ namespace Espera.Mobile.Core.UI
             });
 
             this.ArtistsListView.ItemTapped += async (sender, e) => await this.Navigation.PushAsync(new RemoteSongsPage());
+
+            this.ViewModel.LoadCommand.Execute(null);
         }
 
         object IViewFor.ViewModel
@@ -48,13 +52,6 @@ namespace Espera.Mobile.Core.UI
         {
             get { return (ArtistsViewModel<RemoteSong>)GetValue(ViewModelProperty); }
             set { SetValue(ViewModelProperty, value); }
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            this.ViewModel.LoadCommand.Execute(null);
         }
     }
 }

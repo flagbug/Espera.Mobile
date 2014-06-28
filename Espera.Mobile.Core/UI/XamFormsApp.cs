69e609097cc84bb6d4709f27adb23d4972ebfd85
Dennis Daume <daume.dennis@gmail.com>
2014-06-28 17:59:28 +0200

    Register the proper main thread scheduler

--- Espera.Mobile.Core/UI/XamFormsApp.cs
+++ Espera.Mobile.Core/UI/XamFormsApp.cs
@@ -1,9 +1,15 @@
-﻿using Xamarin.Forms;
+﻿using ReactiveUI;
+using Xamarin.Forms;
 
 namespace Espera.Mobile.Core.UI
 {
     public static class XamFormsApp
     {
+        static XamFormsApp()
+        {
+            RxApp.MainThreadScheduler = new DeviceScheduler();
+        }
+
         public static Page GetMainPage()
         {
             return new NavigationPage(new MainPage());

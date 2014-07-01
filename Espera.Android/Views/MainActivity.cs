6945903ee8e08af4c2c731a663076fe1ea731f32
Dennis Daume <daume.dennis@gmail.com>
2014-07-01 15:18:12 +0200

    Fix the registrations

--- Espera.Android/Views/MainActivity.cs
+++ Espera.Android/Views/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.OS;
 using Espera.Android.Services;
 using Espera.Mobile.Core.UI;
 using Google.Analytics.Tracking;
+using ReactiveUI;
+using ReactiveUI.XamForms;
+using Splat;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
@@ -20,6 +23,10 @@ namespace Espera.Android.Views
             base.OnCreate(bundle);
 
             Forms.Init(this, bundle);
+
+            Locator.CurrentMutable.Register(() => new ActivationForViewFetcher(), typeof(IActivationForViewFetcher));
+            Locator.CurrentMutable.Register(() => new XamlDefaultPropertyBinding(), typeof(IDefaultPropertyBindingProvider));
+            RxApp.MainThreadScheduler = new DeviceScheduler();
 
             this.SetPage(XamFormsApp.GetMainPage());
 
--- Espera.Mobile.Core/UI/XamFormsApp.cs
+++ Espera.Mobile.Core/UI/XamFormsApp.cs
@@ -1,16 +1,10 @@
-﻿using ReactiveUI;
-using Splat;
+﻿using Splat;
 using Xamarin.Forms;
 
 namespace Espera.Mobile.Core.UI
 {
     public static class XamFormsApp
     {
-        static XamFormsApp()
-        {
-            RxApp.MainThreadScheduler = new DeviceScheduler();
-        }
-
         public static INotification Notifications
         {
             get { return Locator.Current.GetService<INotification>(); }

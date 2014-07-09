3b2049033fd154bcef942fe5b450da79d75d8e14
Dennis Daume <daume.dennis@gmail.com>
2014-07-09 21:07:01 +0200

    Xamarin.Forms isn't ready yet. Revert to native Android controls

--- Espera.Android/Views/MainActivity.cs
+++ Espera.Android/Views/MainActivity.cs
@@ -1,25 +1,110 @@
-﻿using Android.App;
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.Net.Wifi;
 using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Espera.Android.Analytics;
 using Espera.Android.Services;
-using Espera.Mobile.Core.UI;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.Android;
+using Espera.Mobile.Core.Analytics;
+using Espera.Mobile.Core.Network;
+using Espera.Mobile.Core.ViewModels;
+using Google.Analytics.Tracking;
+using ReactiveMarrow;
+using ReactiveUI;
+using Splat;
+using IMenuItem = Android.Views.IMenuItem;
 
 namespace Espera.Android.Views
 {
     [Activity(Label = "Espera", MainLauncher = true, Icon = "@drawable/icon",
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleTop)]
-    public class MainActivity : AndroidActivity
+        ConfigurationChanges = ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleTop)]
+    public class MainActivity : ReactiveActivity<MainViewModel>
     {
+        public MainActivity()
+        {
+            var wifiService = Locator.Current.GetService<IWifiService>();
+            this.ViewModel = new MainViewModel(wifiService.GetIpAddress);
+
+            this.WhenActivated(() =>
+            {
+                var disposable = new CompositeDisposable();
+
+                var connectOrDisconnectCommand = this.ViewModel.WhenAnyValue(x => x.IsConnected)
+                    .Select(x => x ? (IReactiveCommand)this.ViewModel.DisconnectCommand : this.ViewModel.ConnectCommand);
+
+                connectOrDisconnectCommand.SampleAndCombineLatest(this.ConnectButton.Events().Click, (command, args) => command)
+                    .Where(x => x.CanExecute(null))
+                    .Subscribe(x => x.Execute(null))
+                    .DisposeWith(disposable);
+
+                connectOrDisconnectCommand.SelectMany(x => x.CanExecuteObservable)
+                    .BindTo(this.ConnectButton, x => x.Enabled)
+                    .DisposeWith(disposable);
+
+                this.ViewModel.ConnectCommand.IsExecuting
+                    .CombineLatest(this.ViewModel.WhenAnyValue(x => x.IsConnected), (connecting, connected) =>
+                        connected ? "Disconnect" : connecting ? "Connecting..." : "Connect")
+                    .BindTo(this.ConnectButton, x => x.Text)
+                    .DisposeWith(disposable);
+
+                this.ViewModel.ConnectionFailed
+                    .Subscribe(x => Toast.MakeText(this, x, ToastLength.Long).Show())
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(this.ViewModel, x => x.IsConnected, x => x.LoadArtistsButton.Enabled)
+                    .DisposeWith(disposable);
+                this.LoadArtistsButton.Events().Click.Subscribe(x => this.StartActivity(typeof(RemoteArtistsActivity)))
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(this.ViewModel, x => x.IsConnected, x => x.LoadCurrentPlaylistButton.Enabled)
+                    .DisposeWith(disposable);
+                this.LoadCurrentPlaylistButton.Events().Click.Subscribe(x => this.StartActivity(typeof(PlaylistActivity)))
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(this.ViewModel, x => x.IsConnected, x => x.LocalSongsButton.Enabled);
+                this.LocalSongsButton.Events().Click.Subscribe(x => this.StartActivity(typeof(LocalArtistsActivity)))
+                    .DisposeWith(disposable); ;
+
+                return disposable;
+            });
+        }
+
+        public Button ConnectButton { get; private set; }
+
+        public Button LoadArtistsButton { get; private set; }
+
+        public Button LoadCurrentPlaylistButton { get; private set; }
+
+        public Button LocalSongsButton { get; private set; }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            menu.Add("Settings").SetIcon(Resource.Drawable.Settings)
+                .SetShowAsAction(ShowAsAction.Always);
+
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            this.StartActivity(typeof(SettingsActivity));
+
+            return true;
+        }
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            Forms.Init(this, bundle);
+            this.Title = String.Empty;
 
-            this.SetPage(XamFormsApp.GetMainPage());
+            this.SetContentView(Resource.Layout.Main);
+            this.WireUpControls();
 
             this.StartService(new Intent(this, typeof(NetworkService)));
         }
@@ -30,7 +115,6 @@ namespace Espera.Android.Views
             this.Intent = intent;
         }
 
-        /*
         protected override void OnResume()
         {
             base.OnResume();
@@ -55,10 +139,32 @@ namespace Espera.Android.Views
                 var analytics = new AndroidAnalytics(this.ApplicationContext);
                 int wifiSpeed = info.LinkSpeed;
                 analytics.RecordWifiSpeed(wifiSpeed);
-
-                this.ViewModel.LocalAddress = new IPAddress(info.IpAddress);
             }
         }
-         * */
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            EasyTracker.GetInstance(this).ActivityStart(this);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            EasyTracker.GetInstance(this).ActivityStop(this);
+        }
+
+        private void ShowWifiPrompt(WifiManager wifiManager)
+        {
+            var builder = new AlertDialog.Builder(this);
+            builder.SetTitle("Error");
+            builder.SetMessage("You have to enable Wifi.");
+            builder.SetPositiveButton("Enable", (sender, args) => wifiManager.SetWifiEnabled(true));
+            builder.SetNegativeButton("Exit", (sender, args) => this.Finish());
+
+            builder.Show();
+        }
     }
 }
---
+++ Espera.Android/Views/PlaylistActivity.cs
@@ -0,0 +1,198 @@
+using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Espera.Mobile.Core.ViewModels;
+using Google.Analytics.Tracking;
+using ReactiveMarrow;
+using ReactiveUI;
+
+namespace Espera.Android.Views
+{
+    [Activity(Label = "Current Playlist", ConfigurationChanges = ConfigChanges.Orientation)]
+    public class PlaylistActivity : ReactiveActivity<PlaylistViewModel>
+    {
+        private ProgressDialog progressDialog;
+
+        public PlaylistActivity()
+        {
+            this.WhenActivated(() =>
+            {
+                var disposable = new CompositeDisposable();
+
+                this.ViewModel.Message.Subscribe(x => Toast.MakeText(this, x, ToastLength.Short).Show())
+                    .DisposeWith(disposable);
+
+                var adapter = new ReactiveListAdapter<PlaylistEntryViewModel>(this.ViewModel.Entries, (vm, parent) => new PlaylistEntryView(this, vm, parent))
+                    .DisposeWith(disposable);
+                this.Playlist.Adapter = adapter;
+
+                this.Playlist.Events().ItemClick.Select(x => x.Position)
+                    .Subscribe(x =>
+                    {
+                        this.ViewModel.SelectedEntry = this.ViewModel.Entries[x];
+
+                        bool hasVotesLeft = this.ViewModel.RemainingVotes > 0;
+                        string voteString = hasVotesLeft ? string.Format("Vote ({0})", this.ViewModel.RemainingVotes) : "No votes left";
+
+                        if (this.ViewModel.CanModify)
+                        {
+                            var builder = new AlertDialog.Builder(this);
+                            var items = new List<string> { "Play", "Remove", "Move Up", "Move Down" };
+
+                            if (this.ViewModel.CanVoteOnSelectedEntry)
+                            {
+                                items.Add(voteString);
+                            }
+
+                            builder.SetItems(items.ToArray(), async (o, eventArgs) =>
+                            {
+                                switch (eventArgs.Which)
+                                {
+                                    case 0:
+                                        await this.ViewModel.PlayPlaylistSongCommand.ExecuteAsync();
+                                        break;
+
+                                    case 1:
+                                        await this.ViewModel.RemoveSongCommand.ExecuteAsync();
+                                        break;
+
+                                    case 2:
+                                        await this.ViewModel.MoveSongUpCommand.ExecuteAsync();
+                                        break;
+
+                                    case 3:
+                                        await this.ViewModel.MoveSongDownCommand.ExecuteAsync();
+                                        break;
+
+                                    case 4:
+                                        if (hasVotesLeft)
+                                        {
+                                            await this.ViewModel.VoteCommand.ExecuteAsync();
+                                        }
+                                        break;
+                                }
+                            });
+                            builder.Create().Show();
+                        }
+
+                        else if (this.ViewModel.CanVoteOnSelectedEntry)
+                        {
+                            var builder = new AlertDialog.Builder(this);
+                            builder.SetItems(new[] { voteString }, async (sender, args) =>
+                            {
+                                if (hasVotesLeft)
+                                {
+                                    await this.ViewModel.VoteCommand.ExecuteAsync();
+                                }
+                            });
+                            builder.Create().Show();
+                        }
+                    }).DisposeWith(disposable);
+                this.Playlist.EmptyView = this.FindViewById(global::Android.Resource.Id.Empty);
+
+                this.ViewModel.WhenAnyValue(x => x.CanModify).Select(x => x ? ViewStates.Visible : ViewStates.Gone)
+                    .BindTo(this.PlaybackControlPanel, x => x.Visibility)
+                    .DisposeWith(disposable);
+
+                this.BindCommand(this.ViewModel, x => x.PlayNextSongCommand, x => x.NextButton)
+                    .DisposeWith(disposable);
+                this.BindCommand(this.ViewModel, x => x.PlayPreviousSongCommand, x => x.PreviousButton)
+                    .DisposeWith(disposable);
+                this.BindCommand(this.ViewModel, x => x.PlayPauseCommand, x => x.PlayPauseButton)
+                    .DisposeWith(disposable);
+
+                this.ViewModel.WhenAnyValue(x => x.IsPlaying).Select(x => x ? Resource.Drawable.Pause : Resource.Drawable.Play)
+                    .Subscribe(x => this.PlayPauseButton.SetBackgroundResource(x))
+                    .DisposeWith(disposable);
+
+                Func<bool, int> alphaSelector = x => x ? 255 : 100;
+
+                this.ViewModel.PlayPauseCommand.CanExecuteObservable.Select(alphaSelector)
+                    .Subscribe(x => this.PlayPauseButton.Background.SetAlpha(x))
+                    .DisposeWith(disposable);
+
+                this.ViewModel.PlayPreviousSongCommand.CanExecuteObservable.Select(alphaSelector)
+                    .Subscribe(x => this.PreviousButton.Background.SetAlpha(x))
+                    .DisposeWith(disposable);
+
+                this.ViewModel.PlayNextSongCommand.CanExecuteObservable.Select(alphaSelector)
+                    .Subscribe(x => this.NextButton.Background.SetAlpha(x))
+                    .DisposeWith(disposable);
+
+                this.progressDialog = new ProgressDialog(this);
+                this.progressDialog.SetMessage("Loading playlist");
+                this.progressDialog.Indeterminate = true;
+                this.progressDialog.SetCancelable(false);
+
+                this.ViewModel.LoadPlaylistCommand.IsExecuting
+                    .Skip(1)
+                    .Subscribe(x =>
+                    {
+                        if (x)
+                        {
+                            this.progressDialog.Show();
+                        }
+
+                        else if (this.progressDialog.IsShowing)
+                        {
+                            this.progressDialog.Dismiss();
+                        }
+                    }).DisposeWith(disposable);
+
+                this.ViewModel.LoadPlaylistCommand.Execute(null);
+
+                return disposable;
+            });
+        }
+
+        public Button NextButton { get; private set; }
+
+        public LinearLayout PlaybackControlPanel { get; private set; }
+
+        public ListView Playlist { get; private set; }
+
+        public Button PlayPauseButton { get; private set; }
+
+        public Button PreviousButton { get; private set; }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            this.SetContentView(Resource.Layout.Playlist);
+            this.WireUpControls();
+
+            this.ViewModel = new PlaylistViewModel();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            if (this.progressDialog != null && this.progressDialog.IsShowing)
+            {
+                this.progressDialog.Dismiss();
+            }
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            EasyTracker.GetInstance(this).ActivityStart(this);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            EasyTracker.GetInstance(this).ActivityStop(this);
+        }
+    }
+}

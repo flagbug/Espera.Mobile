85cabec5d70f81f58fededcb3ab0f379dad08dd7
Dennis Daume <daume.dennis@gmail.com>
2014-07-10 21:26:01 +0200

    Dispose supscriptions properly

--- Espera.Android/Views/RemoteSongsActivity.cs
+++ Espera.Android/Views/RemoteSongsActivity.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Akavache;
 using Android.App;
@@ -12,6 +13,7 @@ using Espera.Mobile.Core;
 using Espera.Mobile.Core.Songs;
 using Espera.Mobile.Core.ViewModels;
 using Google.Analytics.Tracking;
+using ReactiveMarrow;
 using ReactiveUI;
 
 namespace Espera.Android.Views
@@ -19,6 +21,48 @@ namespace Espera.Android.Views
     [Activity(ConfigurationChanges = ConfigChanges.Orientation)]
     public class RemoteSongsActivity : ReactiveActivity<RemoteSongsViewModel>
     {
+        public RemoteSongsActivity()
+        {
+            var songs = BlobCache.InMemory.GetObjectAsync<IEnumerable<RemoteSong>>(BlobCacheKeys.SelectedRemoteSongs).Wait().ToList();
+
+            this.Title = songs.First().Artist;
+            this.ViewModel = new RemoteSongsViewModel(songs);
+
+            this.WhenActivated(() =>
+            {
+                var disposable = new CompositeDisposable();
+
+                this.SongsList.Adapter = new RemoteSongsAdapter(this, new ReactiveList<RemoteSong>(this.ViewModel.Songs));
+                this.SongsList.Events().ItemClick.Select(x => x.Position)
+                    .Subscribe(x =>
+                    {
+                        this.ViewModel.SelectedSong = this.ViewModel.Songs[x];
+
+                        var builder = new AlertDialog.Builder(this);
+                        builder.SetItems(new[] { "Play", "Add to playlist" }, async (o, eventArgs) =>
+                        {
+                            switch (eventArgs.Which)
+                            {
+                                case 0:
+                                    await this.ViewModel.PlaySongsCommand.ExecuteAsync();
+                                    break;
+
+                                case 1:
+                                    await this.ViewModel.AddToPlaylistCommand.ExecuteAsync();
+                                    break;
+                            }
+                        });
+                        builder.Create().Show();
+                    })
+                    .DisposeWith(disposable);
+
+                this.ViewModel.Messages.Subscribe(x => Toast.MakeText(this, x, ToastLength.Short).Show())
+                    .DisposeWith(disposable);
+
+                return disposable;
+            });
+        }
+
         public ListView SongsList { get; private set; }
 
         public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
@@ -32,36 +76,6 @@ namespace Espera.Android.Views
 
             this.SetContentView(Resource.Layout.RemoteSongs);
             this.WireUpControls();
-
-            var songs = BlobCache.InMemory.GetObjectAsync<IEnumerable<RemoteSong>>(BlobCacheKeys.SelectedRemoteSongs).Wait().ToList();
-
-            this.Title = songs.First().Artist;
-            this.ViewModel = new RemoteSongsViewModel(songs);
-
-            this.SongsList.Adapter = new RemoteSongsAdapter(this, new ReactiveList<RemoteSong>(this.ViewModel.Songs));
-            this.SongsList.Events().ItemClick.Select(x => x.Position)
-                .Subscribe(x =>
-                {
-                    this.ViewModel.SelectedSong = this.ViewModel.Songs[x];
-
-                    var builder = new AlertDialog.Builder(this);
-                    builder.SetItems(new[] { "Play", "Add to playlist" }, async (o, eventArgs) =>
-                    {
-                        switch (eventArgs.Which)
-                        {
-                            case 0:
-                                await this.ViewModel.PlaySongsCommand.ExecuteAsync();
-                                break;
-
-                            case 1:
-                                await this.ViewModel.AddToPlaylistCommand.ExecuteAsync();
-                                break;
-                        }
-                    });
-                    builder.Create().Show();
-                });
-
-            this.ViewModel.Messages.Subscribe(x => Toast.MakeText(this, x, ToastLength.Short).Show());
         }
 
         protected override void OnStart()

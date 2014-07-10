97457cd9c36a63400d4da7dfd8b91e7f78d86e1d
Dennis Daume <daume.dennis@gmail.com>
2014-07-10 21:26:01 +0200

    Dispose supscriptions properly

--- Espera.Android/Views/LocalSongsActivity.cs
+++ Espera.Android/Views/LocalSongsActivity.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Akavache;
 using Android.App;
 using Android.Content.PM;
@@ -8,17 +13,55 @@ using Espera.Mobile.Core;
 using Espera.Mobile.Core.Songs;
 using Espera.Mobile.Core.ViewModels;
 using Google.Analytics.Tracking;
+using ReactiveMarrow;
 using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Linq;
 
 namespace Espera.Android.Views
 {
     [Activity(ConfigurationChanges = ConfigChanges.Orientation)]
     public class LocalSongsActivity : ReactiveActivity<LocalSongsViewModel>
     {
+        public LocalSongsActivity()
+        {
+            var songs = BlobCache.InMemory.GetObjectAsync<IEnumerable<LocalSong>>(BlobCacheKeys.SelectedLocalSongs).Wait().ToList();
+            this.ViewModel = new LocalSongsViewModel(songs);
+            this.Title = songs.First().Artist;
+
+            this.WhenActivated(() =>
+            {
+                var disposable = new CompositeDisposable();
+
+                var adapter = new ReactiveListAdapter<LocalSongViewModel>(new ReactiveList<LocalSongViewModel>(this.ViewModel.Songs),
+                    (vm, parent) => new LocalSongView(this, vm, parent));
+                this.SongsList.Adapter = adapter;
+
+                this.SongsList.Events().ItemClick.Select(x => x.Position)
+                    .Subscribe(x =>
+                    {
+                        this.ViewModel.SelectedSong = this.ViewModel.Songs[x];
+
+                        var builder = new AlertDialog.Builder(this);
+                        builder.SetItems(new[] { "Add to playlist" }, async (o, eventArgs) =>
+                        {
+                            switch (eventArgs.Which)
+                            {
+                                case 0:
+                                    await this.ViewModel.AddToPlaylistCommand.ExecuteAsync();
+                                    break;
+                            }
+                        });
+
+                        builder.Create().Show();
+                    })
+                    .DisposeWith(disposable);
+
+                this.ViewModel.Messages.Subscribe(x => Toast.MakeText(this, x, ToastLength.Short).Show())
+                    .DisposeWith(disposable);
+
+                return disposable;
+            });
+        }
+
         public ListView SongsList { get; private set; }
 
         public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
@@ -32,36 +75,6 @@ namespace Espera.Android.Views
 
             this.SetContentView(Resource.Layout.LocalSongs);
             this.WireUpControls();
-
-            var songs = BlobCache.InMemory.GetObjectAsync<IEnumerable<LocalSong>>(BlobCacheKeys.SelectedLocalSongs).Wait().ToList();
-
-            this.Title = songs.First().Artist;
-            this.ViewModel = new LocalSongsViewModel(songs);
-
-            var adapter = new ReactiveListAdapter<LocalSongViewModel>(new ReactiveList<LocalSongViewModel>(this.ViewModel.Songs),
-                (vm, parent) => new LocalSongView(this, vm, parent));
-            this.SongsList.Adapter = adapter;
-
-            this.SongsList.Events().ItemClick.Select(x => x.Position)
-                .Subscribe(x =>
-                {
-                    this.ViewModel.SelectedSong = this.ViewModel.Songs[x];
-
-                    var builder = new AlertDialog.Builder(this);
-                    builder.SetItems(new[] { "Add to playlist" }, async (o, eventArgs) =>
-                    {
-                        switch (eventArgs.Which)
-                        {
-                            case 0:
-                                await this.ViewModel.AddToPlaylistCommand.ExecuteAsync();
-                                break;
-                        }
-                    });
-
-                    builder.Create().Show();
-                });
-
-            this.ViewModel.Messages.Subscribe(x => Toast.MakeText(this, x, ToastLength.Short).Show());
         }
 
         protected override void OnStart()

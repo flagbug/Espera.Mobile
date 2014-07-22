06c2889bc4ed1a1f94454ff250b4888de872f3f8
Dennis Daume <daume.dennis@gmail.com>
2014-07-23 00:53:35 +0200

    Fix the build script
    Google decided to move things around

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -6,4 +6,4 @@ $MSBuildLocation = "C:\Program Files (x86)\MSBuild\12.0\bin"
 & 'C:\Program Files (x86)\Java\jdk1.6.0_39\bin\jarsigner.exe' -verbose -sigalg SHA1withRSA -digestalg SHA1  -keystore ./Espera.Mobile.keystore -signedjar ./Espera.Android/bin/Release/com.flagbug.esperamobile-signed.apk ./Espera.Android/bin/Release/com.flagbug.esperamobile.apk Espera
 
 mkdir -Force ./Release
-& 'C:\Program Files (x86)\Android\android-sdk\tools\zipalign.exe' -f -v 4 ./Espera.Android/bin/Release/com.flagbug.esperamobile-signed.apk ./Release/Espera.Android.apk
+& 'C:\Program Files (x86)\Android\android-sdk\build-tools\20.0.0\zipalign.exe' -f -v 4 ./Espera.Android/bin/Release/com.flagbug.esperamobile-signed.apk ./Release/Espera.Android.apk

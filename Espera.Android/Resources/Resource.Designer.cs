39efa00c0ee00cf35b08b645cb6e119ac9f85e03
Dennis Daume <daume.dennis@gmail.com>
2014-08-26 16:18:36 +0200

    Added a trial expiration notice

--- Espera.Android/Resources/Resource.Designer.cs
+++ Espera.Android/Resources/Resource.Designer.cs
@@ -120,14 +120,14 @@ namespace Espera.Android
 			// aapt resource value: 0x7f070005
 			public const int ConnectButton = 2131165189;
 			
-			// aapt resource value: 0x7f07000b
-			public const int CurrentTimeTextView = 2131165195;
-			
 			// aapt resource value: 0x7f07000c
-			public const int DurationSeekBar = 2131165196;
+			public const int CurrentTimeTextView = 2131165196;
 			
-			// aapt resource value: 0x7f070011
-			public const int Image = 2131165201;
+			// aapt resource value: 0x7f07000d
+			public const int DurationSeekBar = 2131165197;
+			
+			// aapt resource value: 0x7f070012
+			public const int Image = 2131165202;
 			
 			// aapt resource value: 0x7f070008
 			public const int LoadLocalArtistsButton = 2131165192;
@@ -147,32 +147,35 @@ namespace Espera.Android
 			// aapt resource value: 0x7f070003
 			public const int LocalSongTransferProgress = 2131165187;
 			
-			// aapt resource value: 0x7f070010
-			public const int NextButton = 2131165200;
-			
-			// aapt resource value: 0x7f070009
-			public const int PlayList = 2131165193;
-			
-			// aapt resource value: 0x7f07000f
-			public const int PlayPauseButton = 2131165199;
+			// aapt resource value: 0x7f070011
+			public const int NextButton = 2131165201;
 			
 			// aapt resource value: 0x7f07000a
-			public const int PlaybackControlPanel = 2131165194;
+			public const int PlayList = 2131165194;
+			
+			// aapt resource value: 0x7f070010
+			public const int PlayPauseButton = 2131165200;
+			
+			// aapt resource value: 0x7f07000b
+			public const int PlaybackControlPanel = 2131165195;
+			
+			// aapt resource value: 0x7f070014
+			public const int PlaylistEntryArtist = 2131165204;
 			
 			// aapt resource value: 0x7f070013
-			public const int PlaylistEntryArtist = 2131165203;
+			public const int PlaylistEntryTitle = 2131165203;
 			
-			// aapt resource value: 0x7f070012
-			public const int PlaylistEntryTitle = 2131165202;
-			
-			// aapt resource value: 0x7f07000e
-			public const int PreviousButton = 2131165198;
+			// aapt resource value: 0x7f07000f
+			public const int PreviousButton = 2131165199;
 			
 			// aapt resource value: 0x7f070004
 			public const int SongsList = 2131165188;
 			
-			// aapt resource value: 0x7f07000d
-			public const int TotalTimeTextView = 2131165197;
+			// aapt resource value: 0x7f07000e
+			public const int TotalTimeTextView = 2131165198;
+			
+			// aapt resource value: 0x7f070009
+			public const int TrialExpirationTextView = 2131165193;
 			
 			static Id()
 			{
@@ -280,6 +283,9 @@ namespace Espera.Android
 			
 			// aapt resource value: 0x7f04000c
 			public const int preference_save_energy_title = 2130968588;
+			
+			// aapt resource value: 0x7f040013
+			public const int trial_expiration = 2130968595;
 			
 			static String()
 			{
--- Espera.Mobile.Core/TrialHelpers.cs
+++ Espera.Mobile.Core/TrialHelpers.cs
@@ -5,12 +5,17 @@ namespace Espera.Mobile.Core
 {
     public static class TrialHelpers
     {
-        public static bool IsInTrialPeriod(TimeSpan trialTime, IClock clock = null, IInstallationDateFetcher installationDateFetcher = null)
+        public static TimeSpan GetRemainingTrialTime(TimeSpan trialTime, IClock clock = null, IInstallationDateFetcher installationDateFetcher = null)
         {
             clock = clock ?? new Clock();
             installationDateFetcher = installationDateFetcher ?? Locator.Current.GetService<IInstallationDateFetcher>();
 
-            return installationDateFetcher.GetInstallationDate() + trialTime > clock.Now;
+            return installationDateFetcher.GetInstallationDate() + trialTime - clock.Now;
+        }
+
+        public static bool IsInTrialPeriod(TimeSpan trialTime, IClock clock = null, IInstallationDateFetcher installationDateFetcher = null)
+        {
+            return GetRemainingTrialTime(trialTime, clock, installationDateFetcher) > TimeSpan.Zero;
         }
     }
 }

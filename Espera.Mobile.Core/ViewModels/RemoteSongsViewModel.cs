7ac91f0c8fd2a5552e068dda7443948a6dcf74b9
Dennis Daume <daume.dennis@gmail.com>
2014-08-30 20:08:58 +0200

    Implement the guest system for remote songs

--- Espera.Mobile.Core/ViewModels/RemoteSongsViewModel.cs
+++ Espera.Mobile.Core/ViewModels/RemoteSongsViewModel.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Reactive.Disposables;
 using Espera.Mobile.Core.Network;
 using Espera.Mobile.Core.Songs;
 using Espera.Network;
+using ReactiveMarrow;
 using ReactiveUI;
 using System;
 using System.Linq;
@@ -9,8 +11,10 @@ using System.Reactive.Linq;
 
 namespace Espera.Mobile.Core.ViewModels
 {
-    public class RemoteSongsViewModel : ReactiveObject
+    public class RemoteSongsViewModel : ReactiveObject, ISupportsActivation
     {
+        private ObservableAsPropertyHelper<bool> isAdmin;
+        private ObservableAsPropertyHelper<int?> remainingVotes;
         private RemoteSong selectedSong;
 
         public RemoteSongsViewModel(IReadOnlyList<RemoteSong> songs)
@@ -19,6 +23,8 @@ namespace Espera.Mobile.Core.ViewModels
                 throw new ArgumentNullException("songs");
 
             this.Songs = songs.Order().ToList();
+
+            this.Activator = new ViewModelActivator();
 
             this.PlaySongsCommand = ReactiveCommand.CreateAsyncTask(x => NetworkMessenger.Instance.PlaySongsAsync(
                 this.Songs.SkipWhile(song => song.Guid != this.SelectedSong.Guid).Select(y => y.Guid).ToList()));
@@ -31,13 +37,41 @@ namespace Espera.Mobile.Core.ViewModels
 
             this.Messages = playSongsMessage.Merge(addToPlaylistMessage)
                 .ObserveOn(RxApp.MainThreadScheduler);
+
+            this.WhenActivated(() =>
+            {
+                var disposable = new CompositeDisposable();
+
+                this.remainingVotes = NetworkMessenger.Instance.WhenAnyValue(x => x.GuestSystemInfo)
+                    .Select(x => x.IsEnabled ? new int?(x.RemainingVotes) : null)
+                    .ToProperty(this, x => x.RemainingVotes)
+                    .DisposeWith(disposable);
+
+                this.isAdmin = NetworkMessenger.Instance.WhenAnyValue(x => x.AccessPermission, x => x == NetworkAccessPermission.Admin)
+                    .ToProperty(this, x => x.IsAdmin)
+                    .DisposeWith(disposable);
+
+                return disposable;
+            });
         }
 
+        public ViewModelActivator Activator { get; private set; }
+
         public ReactiveCommand<ResponseInfo> AddToPlaylistCommand { get; private set; }
+
+        public bool IsAdmin
+        {
+            get { return this.isAdmin.Value; }
+        }
 
         public IObservable<string> Messages { get; private set; }
 
         public ReactiveCommand<ResponseInfo> PlaySongsCommand { get; private set; }
+
+        public int? RemainingVotes
+        {
+            get { return this.remainingVotes.Value; }
+        }
 
         public RemoteSong SelectedSong
         {

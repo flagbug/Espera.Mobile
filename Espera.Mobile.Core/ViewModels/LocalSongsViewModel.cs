4853650b28ed8d15efd66a1352211a0e4e4ebfb7
Dennis Daume <daume.dennis@gmail.com>
2014-08-31 16:12:15 +0200

    Implement the guest system for local songs

--- Espera.Mobile.Core/ViewModels/LocalSongsViewModel.cs
+++ Espera.Mobile.Core/ViewModels/LocalSongsViewModel.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Espera.Mobile.Core.Network;
 using Espera.Mobile.Core.Songs;
+using Espera.Network;
+using ReactiveMarrow;
 using ReactiveUI;
 using Splat;
 
 namespace Espera.Mobile.Core.ViewModels
 {
-    public class LocalSongsViewModel : ReactiveObject
+    public class LocalSongsViewModel : ReactiveObject, ISupportsActivation
     {
+        private ObservableAsPropertyHelper<bool> isAdmin;
+        private ObservableAsPropertyHelper<int?> remainingVotes;
         private LocalSongViewModel selectedSong;
 
         public LocalSongsViewModel(IReadOnlyList<LocalSong> songs)
@@ -22,16 +27,46 @@ namespace Espera.Mobile.Core.ViewModels
 
             this.Songs = songs.Order().Select(x => new LocalSongViewModel(x)).ToList();
 
+            this.Activator = new ViewModelActivator();
+
             this.AddToPlaylistCommand = ReactiveCommand.CreateAsyncTask(x => this.QueueSong(this.SelectedSong));
 
             this.Messages = this.AddToPlaylistCommand.ThrownExceptions
                 .Select(_ => "Something went wrong")
                 .ObserveOn(RxApp.MainThreadScheduler);
+
+            this.WhenActivated(() =>
+            {
+                var disposable = new CompositeDisposable();
+
+                this.remainingVotes = NetworkMessenger.Instance.WhenAnyValue(x => x.GuestSystemInfo)
+                    .Select(x => x.IsEnabled ? new int?(x.RemainingVotes) : null)
+                    .ToProperty(this, x => x.RemainingVotes)
+                    .DisposeWith(disposable);
+
+                this.isAdmin = NetworkMessenger.Instance.WhenAnyValue(x => x.AccessPermission, x => x == NetworkAccessPermission.Admin)
+                    .ToProperty(this, x => x.IsAdmin)
+                    .DisposeWith(disposable);
+
+                return disposable;
+            });
         }
+
+        public ViewModelActivator Activator { get; private set; }
 
         public ReactiveCommand<Unit> AddToPlaylistCommand { get; private set; }
 
+        public bool IsAdmin
+        {
+            get { return this.isAdmin.Value; }
+        }
+
         public IObservable<string> Messages { get; private set; }
+
+        public int? RemainingVotes
+        {
+            get { return this.remainingVotes.Value; }
+        }
 
         public LocalSongViewModel SelectedSong
         {

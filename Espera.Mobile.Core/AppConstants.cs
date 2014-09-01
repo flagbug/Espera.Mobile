9fba63870b5d51eae9b602a31a3a67b0618f8697
Dennis Daume <daume.dennis@gmail.com>
2014-09-01 12:37:04 +0200

    Require server version 2.6

--- Espera.Mobile.Core/AppConstants.cs
+++ Espera.Mobile.Core/AppConstants.cs
@@ -4,7 +4,7 @@ namespace Espera.Mobile.Core
 {
     public class AppConstants
     {
-        public static readonly Version MinimumServerVersion = new Version("2.4.0");
+        public static readonly Version MinimumServerVersion = new Version("2.6.0");
         public static readonly TimeSpan TrialTime = TimeSpan.FromDays(7);
     }
 }

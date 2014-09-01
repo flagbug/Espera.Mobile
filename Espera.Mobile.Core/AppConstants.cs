0c46faf1bbd6ad372c863b8d33ee5edf413dd4b1
Dennis Daume <daume.dennis@gmail.com>
2014-09-01 12:36:43 +0200

    Move the minimum server version to the app constants class

--- Espera.Mobile.Core/AppConstants.cs
+++ Espera.Mobile.Core/AppConstants.cs
@@ -4,6 +4,7 @@ namespace Espera.Mobile.Core
 {
     public class AppConstants
     {
+        public static readonly Version MinimumServerVersion = new Version("2.4.0");
         public static readonly TimeSpan TrialTime = TimeSpan.FromDays(7);
     }
 }

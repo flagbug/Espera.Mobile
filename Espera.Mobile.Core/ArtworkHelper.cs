2a573c6eb6b3ce96b3f48f1740b3cd66bd8729e9
Dennis Daume <daume.dennis@gmail.com>
2014-09-12 16:00:44 +0200

    Cache the YouTube and SoundCloud artworks

---
+++ Espera.Mobile.Core/ArtworkHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Akavache;
+using Espera.Network;
+using Fusillade;
+using Splat;
+
+namespace Espera.Mobile.Core
+{
+    public static class ArtworkHelper
+    {
+        public static async Task<IBitmap> LoadArtwork(NetworkSong song)
+        {
+            if (song.ArtworkKey == null)
+                return null;
+
+            byte[] imageBytes = await BlobCache.LocalMachine.GetOrFetchObject(song.ArtworkKey, async () =>
+            {
+                using (var client = new HttpClient(NetCache.UserInitiated))
+                {
+                    return await client.GetByteArrayAsync(song.ArtworkKey);
+                }
+            }, DateTimeOffset.Now + TimeSpan.FromDays(1));
+
+            using (var stream = new MemoryStream(imageBytes))
+            {
+                return await BitmapLoader.Current.Load(stream, null, null);
+            }
+        }
+    }
+}

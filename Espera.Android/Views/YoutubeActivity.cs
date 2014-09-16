f6e9eeec23fe53340b6716dcf237055552907b5a
Dennis Daume <daume.dennis@gmail.com>
2014-09-16 14:42:07 +0200

    Simplified the progress dialog logic

--- Espera.Android/Views/YoutubeActivity.cs
+++ Espera.Android/Views/YoutubeActivity.cs
@@ -50,22 +50,10 @@ namespace Espera.Android.Views
                 this.progressDialog.Indeterminate = true;
                 this.progressDialog.SetCancelable(false);
 
-                this.ViewModel.LoadCommand.IsExecuting
-                    .Skip(1)
-                    .Subscribe(x =>
-                    {
-                        if (x)
-                        {
-                            this.progressDialog.Show();
-                        }
-
-                        else if (this.progressDialog.IsShowing)
-                        {
-                            this.progressDialog.Dismiss();
-                        }
-                    }).DisposeWith(disposable);
+                this.progressDialog.Show();
 
                 this.ViewModel.LoadCommand.ExecuteAsync()
+                    .Finally(() => this.progressDialog.Dismiss())
                     .Subscribe(_ => this.YoutubeSongsList.EmptyView = this.FindViewById(global::Android.Resource.Id.Empty))
                     .DisposeWith(disposable);
 
@@ -109,16 +97,6 @@ namespace Espera.Android.Views
             this.WireUpControls();
 
             this.ViewModel = new YoutubeViewModel();
-        }
-
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
-
-            if (this.progressDialog != null && this.progressDialog.IsShowing)
-            {
-                this.progressDialog.Dismiss();
-            }
         }
 
         protected override void OnStart()

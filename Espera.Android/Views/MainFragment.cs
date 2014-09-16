9485f323b588e8b59e149f61eeca8e4c42ab6c18
Dennis Daume <daume.dennis@gmail.com>
2014-09-16 21:54:54 +0200

    Initial hack on the conversion to fragments and a navigation drawer

---
+++ Espera.Android/Views/MainFragment.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Globalization;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Espera.Mobile.Core;
+using Espera.Mobile.Core.Network;
+using Espera.Mobile.Core.Settings;
+using Espera.Mobile.Core.ViewModels;
+using Espera.Network;
+using Humanizer;
+using ReactiveMarrow;
+using ReactiveUI;
+using Splat;
+
+namespace Espera.Android.Views
+{
+    public class MainFragment : ReactiveFragment<MainViewModel>
+    {
+        public MainFragment()
+        {
+            var settings = Locator.Current.GetService<UserSettings>();
+            var wifiService = Locator.Current.GetService<IWifiService>();
+
+            this.ViewModel = new MainViewModel(settings, wifiService.GetIpAddress);
+
+            this.WhenActivated(() =>
+            {
+                var disposable = new CompositeDisposable();
+
+                var connectOrDisconnectCommand = this.ViewModel.WhenAnyValue(x => x.IsConnected)
+                    .Select(x => x ? (IReactiveCommand)this.ViewModel.DisconnectCommand : this.ViewModel.ConnectCommand);
+
+                connectOrDisconnectCommand.SampleAndCombineLatest(this.ConnectButton.Events().Click, (command, args) => command)
+                    .Where(x => x.CanExecute(null))
+                    .Subscribe(x => x.Execute(null))
+                    .DisposeWith(disposable);
+
+                connectOrDisconnectCommand.SelectMany(x => x.CanExecuteObservable)
+                    .BindTo(this.ConnectButton, x => x.Enabled)
+                    .DisposeWith(disposable);
+
+                this.ViewModel.ConnectCommand.IsExecuting
+                    .CombineLatest(this.ViewModel.WhenAnyValue(x => x.IsConnected), (connecting, connected) =>
+                        connected ? Resource.String.disconnect : connecting ? Resource.String.connecting : Resource.String.connect)
+                    .Select(Resources.GetString)
+                    .BindTo(this.ConnectButton, x => x.Text)
+                    .DisposeWith(disposable);
+
+                this.ViewModel.ConnectCommand.Select(result =>
+                {
+                    switch (result.ConnectionResult)
+                    {
+                        case ConnectionResult.Failed:
+                            return Resources.GetString(Resource.String.connection_failed);
+
+                        case ConnectionResult.ServerVersionToLow:
+                            return string.Format(Resources.GetString(Resource.String.required_server_version), result.ServerVersion.ToString(3));
+
+                        case ConnectionResult.Successful:
+                            {
+                                switch (result.AccessPermission)
+                                {
+                                    case NetworkAccessPermission.Admin:
+                                        return Resources.GetString(Resource.String.connected_as_admin);
+
+                                    case NetworkAccessPermission.Guest:
+                                        return Resources.GetString(Resource.String.connected_as_guest);
+                                }
+                                break;
+                            }
+
+                        case ConnectionResult.Timeout:
+                            return Resources.GetString(Resource.String.connection_timeout);
+
+                        case ConnectionResult.WrongPassword:
+                            return Resources.GetString(Resource.String.wrong_password);
+
+                        case ConnectionResult.WifiDisabled:
+                            return Resources.GetString(Resource.String.wifi_enable_error);
+                    }
+
+                    throw new InvalidOperationException("This shouldn't happen");
+                })
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(x => Toast.MakeText(this.Activity, x, ToastLength.Long).Show())
+                .DisposeWith(disposable);
+
+                /*
+                this.OneWayBind(this.ViewModel, x => x.IsConnected, x => x.LoadPlaylistButton.Enabled)
+                    .DisposeWith(disposable);
+                this.LoadPlaylistButton.Events().Click.Subscribe(x => this.StartActivity(typeof(PlaylistActivity)))
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(this.ViewModel, x => x.IsConnected, x => x.LoadRemoteArtistsButton.Enabled)
+                    .DisposeWith(disposable);
+                this.LoadRemoteArtistsButton.Events().Click.Subscribe(x => this.StartActivity(typeof(RemoteArtistsActivity)))
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(this.ViewModel, x => x.IsConnected, x => x.LoadLocalArtistsButton.Enabled);
+                this.LoadLocalArtistsButton.Events().Click.Subscribe(x => this.StartActivity(typeof(LocalArtistsActivity)))
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(this.ViewModel, x => x.IsConnected, x => x.LoadSoundCloudButton.Enabled);
+                this.LoadSoundCloudButton.Events().Click.Subscribe(x => this.StartActivity(typeof(SoundCloudActivity)))
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(this.ViewModel, x => x.IsConnected, x => x.LoadYoutubeButton.Enabled);
+                this.LoadYoutubeButton.Events().Click.Subscribe(x => this.StartActivity(typeof(YoutubeActivity)))
+                    .DisposeWith(disposable);*/
+
+                bool displayTrialPeriod = !settings.IsPremium;
+                this.TrialExpirationTextView.Visibility = this.TrialExpirationExplanationTextview.Visibility =
+                    displayTrialPeriod ? ViewStates.Visible : ViewStates.Gone;
+                
+                if (displayTrialPeriod)
+                {
+                    TimeSpan remainingTrialTime = TrialHelpers.GetRemainingTrialTime(AppConstants.TrialTime);
+
+                    string expirationMessage = remainingTrialTime > TimeSpan.Zero ?
+                        Resources.GetString(Resource.String.trial_expiration) :
+                        Resources.GetString(Resource.String.trial_expiration_expired);
+
+                    this.TrialExpirationTextView.Text = string.Format("{0} {1}",
+                        expirationMessage,
+                        remainingTrialTime.Humanize(culture: new CultureInfo("en-US")));
+
+                    this.TrialExpirationExplanationTextview.Text = Resources.GetString(Resource.String.trial_expiration_explanation);
+                }
+
+                return disposable;
+            });
+        }
+
+        public Button ConnectButton { get; private set; }
+
+        public Button LoadLocalArtistsButton { get; private set; }
+
+        public Button LoadPlaylistButton { get; private set; }
+
+        public Button LoadRemoteArtistsButton { get; private set; }
+
+        public Button LoadSoundCloudButton { get; private set; }
+
+        public Button LoadYoutubeButton { get; private set; }
+
+        public TextView TrialExpirationExplanationTextview { get; private set; }
+
+        public TextView TrialExpirationTextView { get; private set; }
+
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.MainLayout, null);
+
+            this.WireUpControls(view);
+
+            return view;
+        }
+    }
+}

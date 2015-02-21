122b59ef44be19ac10fc75d38fceaeb6de81aa1d
Dennis Daume <daume.dennis@gmail.com>
2015-02-21 17:30:06 +0100

    MSBuild fiddle

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -2,10 +2,17 @@ param (
     [string]$config = $(throw "config (Release/Dev) is required")
  )
 
-$MSBuildLocation = "C:\Program Files (x86)\MSBuild\14.0\bin"
+Function GetMSBuildExe {
+ 	[CmdletBinding()]
+ 	$DotNetVersion = "4.0"
+ 	$RegKey = "HKLM:\software\Microsoft\MSBuild\ToolsVersions\$DotNetVersion"
+ 	$RegProperty = "MSBuildToolsPath"
+ 	$MSBuildExe = Join-Path -Path (Get-ItemProperty $RegKey).$RegProperty -ChildPath "msbuild.exe"
+ 	Return $MSBuildExe
+ }
 
-& "$MSBuildLocation\MSBuild.exe" Espera.Android\Espera.Android.csproj /p:Configuration=$config /t:Clean
-& "$MSBuildLocation\MSBuild.exe" Espera.Android\Espera.Android.csproj /p:Configuration=$config /t:PackageForAndroid
+&(GetMSBuildExe) Espera.Android\Espera.Android.csproj /p:Configuration=$config /t:Clean
+&(GetMSBuildExe) Espera.Android\Espera.Android.csproj /p:Configuration=$config /t:PackageForAndroid
 
 & 'C:\Program Files (x86)\Java\jdk1.7.0_67\bin\jarsigner.exe' -verbose -sigalg SHA1withRSA -digestalg SHA1  -keystore ./Espera.Mobile.keystore -signedjar ./Espera.Android/bin/$config/com.flagbug.esperamobile-signed.apk ./Espera.Android/bin/$config/com.flagbug.esperamobile.apk Espera
 
